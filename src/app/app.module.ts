5546a4d81000d3c7c962e3bb270c858d7c8012e2
Mahmoud Abdulhadi <mahmoud.abdulhadi1993@gmail.com>
2019-03-13 18:05:35 +0300

    Initial Commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,21 @@
+import { MaterialModule } from './material.module';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+
+
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MaterialModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/material.module.ts
@@ -0,0 +1,17 @@
+import {NgModule} from '@angular/core';
+
+
+
+@NgModule({
+
+
+    imports : [],
+
+    exports : []
+})
+
+export class MaterialModule {
+
+
+
+}
